be796f27fab3336265557e42f4010f184bf7770d
Daniel Rivera <Danielf.r97@gmail.com>
2021-03-17 18:34:13 -0500

    first-deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('/dist/mercadolibre-dr'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/dist/mercadolibre-dr/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
